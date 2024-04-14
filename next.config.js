0c6422d58f47f941cf87481502a1868e54f6159d
jae6269 <jae6269@gmail.com>
2024-04-14 23:23:33 +0900

    [#7]feat: Toast 컴포넌트 구현, mixins,variables 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`, // prependData 옵션 추가
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
