94c18f3403c87bc161c1593e7805a58e92dd4f61
jae6269 <jae6269@gmail.com>
2024-04-16 02:20:08 +0900

    [#4]feat: card 컴포넌트 기본 레이아웃 설정

--- components/commons/card/Card.tsx
+++ components/commons/card/Card.tsx
@@ -3,11 +3,21 @@ import styles from './Card.module.scss';
 
 /**
  * @param {Object} props
- * @param {Object} props.notice "/notices" GET 리스폰스의 "items" 배열속성의 낱개 데이터
+ * @param {Object} props.noticeInfo "/notices" GET 리스폰스의 "items" 배열속성의 낱개 데이터
  */
 
 interface CardProp {
-  notice: CardNoticeType;
+  noticeInfo: CardNoticeType;
 }
 
-export default function Card({ notice }: CardProp) {}
+export default function Card({ noticeInfo }: CardProp) {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.image}></div>
+      <div className={styles.contents}>
+        <div className={styles.shopInfo}></div>
+        <div className={styles.payInfo}></div>
+      </div>
+    </div>
+  );
+}
