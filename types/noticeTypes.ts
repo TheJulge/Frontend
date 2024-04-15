cbaa44bf536fff3b6bba43f73a022e1c86a459f2
jae6269 <jae6269@gmail.com>
2024-04-15 21:41:44 +0900

    [#4]chore: 특정 공고 조회할때 데이터 타입 정의

--- types/noticeTypes.ts
+++ types/noticeTypes.ts
@@ -1,12 +1,4 @@
-//공고 기본 타입
-export interface NoticeItemBaseType {
-  id: string;
-  hourlyPay: number;
-  startsAt: string;
-  workhour: number;
-  description: string;
-  closed: boolean;
-}
+import { ShopBaseType } from './shopTypes';
 
 //links 의 각 요소의 타입
 interface LinkType {
@@ -14,4 +6,45 @@ interface LinkType {
   description: string;
   method: string;
   href: string;
+}
+
+//가게의 특정 공고를 조회할때 "item" 내부에 currentUserApplication 데이터 타입
+interface CurrentUserApplicationItemType {
+  id: string;
+  status: 'pending' | 'accepted' | 'rejected' | 'canceled';
+  createdAt: string;
+}
+
+//특정 공고를 조회할때 "item" 에 들어가는 타입
+export interface NoticeItemType {
+  id: string;
+  hourlyPay: number;
+  startsAt: string;
+  workhour: number;
+  description: string;
+  closed: boolean;
+  shop: {
+    item: ShopBaseType;
+    href: string;
+  };
+  currentUserApplication: {
+    item: CurrentUserApplicationItemType;
+  };
+}
+
+//특정 공고를 조회할때의 데이터 타입
+// GET "/shops/{shop_id}/notices/{notice_id}" 요청성공시 반환하는 데이터 타입
+export interface SingleNoticeType {
+  item: NoticeItemType;
+  links: LinkType[];
+}
+
+//가게의 공고 목록을 조회할때의 데이터타입
+//GET "/shops/{shop_id}/notices" 요청 성공시 반환하는 데이터 타입
+export interface NoticesType {
+  offset: number;
+  limit: number;
+  count: number;
+  hasNext: boolean;
+  //아직 타입 미완성
 }
--- types/userTypes.ts
+++ types/userTypes.ts
@@ -1,3 +1,5 @@
+import { ShopBaseType } from './shopTypes';
+
 //유저 기본 타입
 export interface UserBaseType {
   id: string;
@@ -7,4 +9,11 @@ export interface UserBaseType {
   phone?: string;
   address?: string;
   bio?: string;
+}
+
+//유저 정보 타입(아래 인터페이스) 의 item 속성에 들어갈 타입
+export interface UserInfoItemType extends UserBaseType {
+  shop: {
+    item: ShopBaseType;
+  } | null;
 }
