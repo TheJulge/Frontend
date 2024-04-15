c093f2e31fb24814d11a47f2cf8d08f76d080826
Lim Dong <ilhai6@naver.com>
2024-04-15 11:49:18 +0900

    [#2] feat: button 공통 스타일링 작업 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head';
+import Gnb from '@/components/commons/gnb/Gnb.tsx';
 
 export default function Home() {
   return (
-    <Head>
-      <title>theJulge</title>
-    </Head>
+    <>
+      <Head>
+        <title>theJulge</title>
+      </Head>
+      <Gnb />
+    </>
   );
 }
