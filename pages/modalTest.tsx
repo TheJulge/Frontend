5a7321f7f3f39b5496b12e2a18184ccb187e7448
mun-jihye <m84422583@gmail.com>
2024-04-16 14:51:53 +0900

    [#1] feat: alt 추가

--- pages/modalTest.tsx
+++ pages/modalTest.tsx
@@ -1,5 +1,5 @@
-// import InfoModal from '@/components/commons/modal/InfoModal';
-import ChooseModal from '@/components/commons/modal/ChooseModal';
+import InfoModal from '@/components/commons/modal/InfoModal';
+// import ChooseModal from '@/components/commons/modal/ChooseModal';
 import { useState } from 'react';
 
 export default function modalTest() {
@@ -9,15 +9,15 @@ export default function modalTest() {
     console.log(showModal);
   };
   return (
-    // <InfoModal showModal={true} handleClose={handleClose}>
-    //   가게 정보를 등록해주세요
-    // </InfoModal>
-    <ChooseModal
-      showModal={true}
-      handleNo={handleClose}
-      handleYes={() => console.log('yes!!!')}
-    >
-      신청을 거절하시겠어요?
-    </ChooseModal>
+    <InfoModal showModal={true} handleClose={handleClose}>
+      가게 정보를 등록해주세요
+    </InfoModal>
+    // <ChooseModal
+    //   showModal={true}
+    //   handleNo={handleClose}
+    //   handleYes={() => console.log('yes!!!')}
+    // >
+    //   신청을 거절하시겠어요?
+    // </ChooseModal>
   );
 }
