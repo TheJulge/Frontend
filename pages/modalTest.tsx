2e75e912133c4accf272f842c1abd23e7ca6af59
mun-jihye <m84422583@gmail.com>
2024-04-16 23:23:31 +0900

    [#1] refactor: 공통 모달로직 분리

--- pages/modalTest.tsx
+++ pages/modalTest.tsx
@@ -1,4 +1,4 @@
-// import InfoModal from '@/components/commons/modal/InfoModal';
+import InfoModal from '@/components/commons/modal/InfoModal';
 import ChooseModal from '@/components/commons/modal/ChooseModal';
 import { useState } from 'react';
 
@@ -23,12 +23,10 @@ export default function modalTest() {
           신청을 거절하시겠어요?
         </ChooseModal>
       )}
+      {/* <InfoModal showModal={showModal} handleClose={handleClose}>
+        가게 정보를 등록해주세요
+      </InfoModal> */}
+      ;
     </>
   );
-}
-
-{
-  /* <InfoModal showModal={showModal} handleClose={handleClose}>
-          가게 정보를 등록해주세요
-        </InfoModal> */
 }
