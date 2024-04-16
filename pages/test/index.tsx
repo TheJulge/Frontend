e843b66e1d26d0f1c94629db6daf59794b9f0282
janyong <jyr5023@gmail.com>
2024-04-16 15:21:05 +0900

    [#6]style: 스타일수정

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,5 +1,10 @@
 import Table from '@/components/table/Table.tsx';
+import styles from '@/components/table/Table.module.scss';
 
 export default function test() {
-  return <Table />;
+  return (
+    <div className={styles.container}>
+      <Table />
+    </div>
+  );
 }
