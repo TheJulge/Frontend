97e36105a23c48b3ae55588b1141dd7f08d06154
mun-jihye <m84422583@gmail.com>
2024-04-16 15:40:16 +0900

    [#1] fix: 모달 닫기 기능 구현 에러 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <Link
+        <link
           href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
           rel="stylesheet"
           type="text/css"
