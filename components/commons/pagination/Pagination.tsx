d104ff1f2e332e86274b10e04bdfa9eec86d1e49
janyong <jyr5023@gmail.com>
2024-04-17 18:52:54 +0900

    [#17] feat: 페이지네이션 구현 및 스타일

--- components/commons/pagination/Pagination.tsx
+++ components/commons/pagination/Pagination.tsx
@@ -1,53 +1,62 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import LeftButton from '@/public/images/leftButton.svg';
 import LeftButtonOn from '@/public/images/leftButtonOn.svg';
+import RightButtonOn from '@/public/images/rightButtonOn.svg';
 import RightButton from '@/public/images/rightButton.svg';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import listMockData from '@/components/table/mock.data';
 import styles from './Pagination.module.scss';
 
-interface PaginationType {
-  prevPage: number;
-  prevPagenextPage: number;
-}
-const test: number[] = [1, 2, 3, 4, 5, 6, 7];
-// const fetchProducts = async (page: number) => {
-//   // 데이터 가져오기
-// };
-function Pagination(): PaginationType {
-  // const router = useRouter();
-  // const { page } = router.query;
-  // const pages = Number(page);
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedPage, setSelectedPage] = useState<number | null>(null);
-  const data = listMockData.items;
-  const postsPerPage = 5;
-  // const prevPage = pages - 1 > 0 ? pages - 1 : 1;
-  // const nextPage = pages + 1;
+/**
+ *
+ * @param totalCount   api로 받아온 전체 아이템 카운터 갯수
+ * @param itemCount  한번에 출력할 아이템 갯수
+ * @function handlePageClick 페이지 선택시 선택한 숫자로 쿼리가 바뀌고 해당 페이지로 넘어가는 함수
+ */
 
-  const handlePageClick = (pageNumber: number) => {
-    // router.push(`?page=${page}`, undefined, {
-    //   shallow: true,
-    //   scroll: true,
-    // });
+interface PageNationProps {
+  totalCount: number;
+  itemCount: number;
+}
+
+function Pagination({ totalCount, itemCount }: PageNationProps) {
+  const router = useRouter();
+  const { pathname, query } = router;
+  const page = parseInt(query.page as string, 10) || 1;
+  const [selectedPage, setSelectedPage] = useState(page);
+
+  const totalPages = Math.ceil(totalCount / itemCount);
+
+  const isFirstPage = selectedPage === 1;
+  const isLastPage = selectedPage === totalPages;
+
+  const handlePageClick = async (pageNumber: number): Promise<void> => {
     setSelectedPage(pageNumber);
-    setCurrentPage(pageNumber);
+    await router.replace(
+      {
+        pathname,
+        query: { ...query, page: pageNumber.toString() },
+      },
+      undefined,
+      {
+        shallow: false, // getServerSideProps는 같은 주소(pathname)일 경우 한번만 호출되서 이 옵션을 false로 바꿔서 호출되게 변경
+        scroll: true,
+      },
+    );
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.pageBox}>
-        {page === 1 ? (
+        {isFirstPage ? (
           <LeftButton className={styles.icon} />
         ) : (
-          <Link href={`?page=${prevPage}`}>
-            <LeftButtonOn className={styles.icon} />
-          </Link>
+          <LeftButtonOn
+            className={styles.icon}
+            onClick={() => !isFirstPage && handlePageClick(selectedPage - 1)}
+          />
         )}
         <div className={styles.pageNumberBox}>
-          {test.map(num => (
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map(num => (
             <div
               className={`${styles.pageNumber} ${
                 num === selectedPage ? styles.selected : ''
@@ -59,7 +68,14 @@ function Pagination(): PaginationType {
             </div>
           ))}
         </div>
-        <RightButton className={styles.icon} />
+        {isLastPage ? (
+          <RightButton className={styles.icon} />
+        ) : (
+          <RightButtonOn
+            className={styles.icon}
+            onClick={() => !isLastPage && handlePageClick(selectedPage + 1)}
+          />
+        )}
       </div>
     </div>
   );
