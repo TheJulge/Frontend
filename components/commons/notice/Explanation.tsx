b750e952595dcd75ce8fe57661df898996471e52
jae6269 <jae6269@gmail.com>
2024-04-19 00:05:31 +0900

    [#33]feat: 공고 상세 설명 컴포넌트 구현

---
+++ components/commons/notice/Explanation.tsx
@@ -0,0 +1,12 @@
+import styles from './Explanation.module.scss';
+interface ExplanationProp {
+  description: string;
+}
+export default function Explanation({ description }: ExplanationProp) {
+  return (
+    <div className={styles.explanation}>
+      <p className={styles.title}>공고 설명</p>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
+}
---
+++ pages/details/[id]/index.tsx
@@ -0,0 +1,12 @@
+import Gnb from '@/components/commons/gnb/Gnb';
+import Explanation from '@/components/commons/notice/Explanation';
+
+export async function getSeverSideProps() {}
+
+export default function NoticeDetails() {
+  return (
+    <>
+      <Gnb />
+    </>
+  );
+}
