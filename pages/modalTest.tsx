366cc2fa5d2e88eee0cd5ceafa4a0a64514660cd
Sumin <csmxx99@naver.com>
2024-04-18 14:50:53 +0900

    [#3] test page 주석 처리

--- pages/modalTest.tsx
+++ pages/modalTest.tsx
@@ -1,32 +1,32 @@
-import InfoModal from '@/components/commons/modal/InfoModal';
-import ChooseModal from '@/components/commons/modal/ChooseModal';
-import { useState } from 'react';
+// import InfoModal from '@/components/commons/modal/InfoModal';
+// import ChooseModal from '@/components/commons/modal/ChooseModal';
+// import { useState } from 'react';
 
-export default function modalTest() {
-  const [showModal, setShowModal] = useState(false);
-  const handleClose = () => {
-    setShowModal(false);
-    console.log(showModal);
-  };
-  const handleClick = () => {
-    setShowModal(true);
-  };
-  return (
-    <>
-      <button onClick={handleClick}>modal</button>
-      {showModal && (
-        <ChooseModal
-          showModal={true}
-          handleNo={handleClose}
-          handleYes={() => console.log('yes!!!')}
-        >
-          신청을 거절하시겠어요?
-        </ChooseModal>
-      )}
-      {/* <InfoModal showModal={showModal} handleClose={handleClose}>
-        가게 정보를 등록해주세요
-      </InfoModal> */}
-      ;
-    </>
-  );
-}
+// export default function modalTest() {
+//   const [showModal, setShowModal] = useState(false);
+//   const handleClose = () => {
+//     setShowModal(false);
+//     console.log(showModal);
+//   };
+//   const handleClick = () => {
+//     setShowModal(true);
+//   };
+//   return (
+//     <>
+//       <button onClick={handleClick}>modal</button>
+//       {showModal && (
+//         <ChooseModal
+//           showModal={true}
+//           handleNo={handleClose}
+//           handleYes={() => console.log('yes!!!')}
+//         >
+//           신청을 거절하시겠어요?
+//         </ChooseModal>
+//       )}
+//       {/* <InfoModal showModal={showModal} handleClose={handleClose}>
+//         가게 정보를 등록해주세요
+//       </InfoModal> */}
+//       ;
+//     </>
+//   );
+// }
