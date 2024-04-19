5f8cf9624bad6ea63bf3213ef2f484542f2b53cf
janyongs <wldud5023@naver.com>
2024-04-20 00:24:44 +0900

    [#34] feat: 버튼 클릭시 공고리스트 이동 기능추가

--- components/table/emptytable/EmptyTable.tsx
+++ components/table/emptytable/EmptyTable.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import Link from 'next/link';
 import styles from './EmptyTable.module.scss';
 
 function EmptyTable() {
   return (
     <div className={styles.container}>
       <p>아직 신청내역이 없어요.</p>
+      //공고 목록 페이지로 이동
       <button className={styles.button} type="button">
-        공고 보러가기
+        <Link href="/">공고 보러가기</Link>
       </button>
     </div>
   );
