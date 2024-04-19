b60eba6215dcef8c5ed2fb7323e26a7031164e49
Sumin <csmxx99@naver.com>
2024-04-19 15:45:10 +0900

    [#36] feat: signIn 기능 구현

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -2,17 +2,40 @@ import styles from '@/pages/signin/signin.module.scss';
 import Link from 'next/link';
 import SignInput from '@/components/commons/inputs/signInput/SignInput';
 import LogoIcon from '@/public/images/sign/mainlogo.svg';
+import React, { useState } from 'react';
 
 export default function SignIn() {
+  const [emailValue, setEmailValue] = useState<string>('');
+  const [passwordValue, setPasswordValue] = useState<string>('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(emailValue, passwordValue);
+  };
+
   return (
     <div className={styles.background}>
       <div className={styles.container}>
         <LogoIcon className={styles.logoIcon} alt="더 줄게 로고" />
         <div className={styles.box}>
           <form className={styles.signForm}>
-            <SignInput labelName="이메일" inputType="email" />
-            <SignInput labelName="비밀번호" inputType="password" />
-            <button className={styles.submitButton} type="submit">
+            <SignInput
+              labelName="이메일"
+              inputType="email"
+              value={emailValue}
+              setValue={setEmailValue}
+            />
+            <SignInput
+              labelName="비밀번호"
+              inputType="password"
+              value={passwordValue}
+              setValue={setPasswordValue}
+            />
+            <button
+              className={styles.submitButton}
+              type="submit"
+              onClick={handleSubmit}
+            >
               로그인 하기
             </button>
           </form>
