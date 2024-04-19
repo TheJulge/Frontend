a18461e8d229111965872143ec607fc017cfed32
janyongs <wldud5023@naver.com>
2024-04-19 15:42:32 +0900

    [#34] feat: 내 프로필 상세 반응형 구현

--- pages/users/[id]/index.tsx
+++ pages/users/[id]/index.tsx
@@ -1,24 +1,43 @@
 import Gnb from '@/components/commons/gnb/Gnb';
 import EmployeeTable from '@/components/table/EmployeeTable';
 import ProfileCard from '@/components/profilecard/ProfileCard';
+import { NextPage } from 'next';
+import { ApplicationPageProps } from '@/components/table/ssr/employee.ssr';
+import EmptyTable from '@/components/table/emptytable/EmptyTable';
 import styles from './MyPage.module.scss';
 
-function Mypage() {
+export { getServerSideProps } from '@/components/table/ssr/employee.ssr';
+
+// eslint-disable-next-line react/function-component-definition
+const Mypage: NextPage<ApplicationPageProps> = ({
+  totalCount,
+  itemCount,
+  items,
+}) => {
   return (
     <div className={styles.container}>
       <Gnb />
       <div className={styles.wrapper}>
-        <div className={styles.profile}>
-          <h2>내 프로필</h2>
-          <ProfileCard />
+        <div className={styles.profileWrraper}>
+          <div className={styles.profile}>
+            <h2>내 프로필</h2>
+            <ProfileCard />
+          </div>
         </div>
-        <div className={styles.table}>
-          <h2>신청 내역</h2>
-          {/* <EmployeeTable totalCount={} itemCount={} /> */}
+        <div className={styles.tableWrraper}>
+          <div className={styles.table}>
+            <h2>신청 내역</h2>
+            <EmployeeTable
+              totalCount={totalCount}
+              itemCount={itemCount}
+              items={items}
+            />
+            <EmptyTable />
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Mypage;
