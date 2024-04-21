31e45b326360014c99c89fb07f50112d62217031
jae6269 <jae6269@gmail.com>
2024-04-22 00:02:22 +0900

    [#33]design: NoticeDetailsContainer 레이아웃 , DetailCard 디자인 구현

--- components/commons/details/NoticeDetailsContainer.tsx
+++ components/commons/details/NoticeDetailsContainer.tsx
@@ -1,6 +1,7 @@
 import { CardNoticeType } from '@/types/noticeTypes';
 import NoticeDetailsCard from './detailCard/NoticeDetailsCard';
 import styles from './NoticeDetailsContainer.module.scss';
+import Explanation from './Explanation';
 
 interface DetailsProp {
   details: CardNoticeType;
@@ -16,6 +17,7 @@ export default function NoticeDetailsContainer({ details }: DetailsProp) {
         {shopDetails.name}
       </h1>
       <NoticeDetailsCard noticeDetails={noticeDetails} />
+      <Explanation description={noticeDetails.description} />
     </main>
   );
 }
--- components/commons/details/detailCard/NoticeDetailsCard.tsx
+++ components/commons/details/detailCard/NoticeDetailsCard.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
-import styles from './NoticeDetailsCard.module.scss';
+import ClockIcon from '@/public/images/card/clockIcon.svg';
+import LocationIcon from '@/public/images/card/locationIcon.svg';
+import { formatNoticeTime } from '@/utils/noticeDataFormetters';
 import { NoticeBaseType } from '@/types/noticeTypes';
 import PayIncrease from '../../card/payIncrease/PayIncrease';
+import styles from './NoticeDetailsCard.module.scss';
 
 interface NoticeDetailsCardProp {
   noticeDetails: NoticeBaseType;
@@ -11,10 +14,14 @@ export default function NoticeDetailsCard({
   noticeDetails,
 }: NoticeDetailsCardProp) {
   const shopDetails = noticeDetails.shop.item;
+  const [startDate, workHour] = formatNoticeTime(
+    noticeDetails.startsAt,
+    noticeDetails.workhour,
+  );
   return (
     <div className={styles.container}>
       <div className={styles.cardImg}>
-        <Image src={shopDetails.imageUrl} alt="shop image" layout="fill" />
+        <Image src={shopDetails.imageUrl} alt="shop image" fill />
       </div>
       <div className={styles.contents}>
         <div className={styles.information}>
@@ -23,8 +30,21 @@ export default function NoticeDetailsCard({
             <PayIncrease
               hourlyPay={noticeDetails.hourlyPay}
               originalHourlyPay={shopDetails.originalHourlyPay}
+              closed={false}
             />
           </div>
+          <div className={styles.time}>
+            <ClockIcon />
+            <p>
+              {startDate}
+              {workHour}
+            </p>
+          </div>
+          <div className={styles.location}>
+            <LocationIcon />
+            <p>{shopDetails.address1}</p>
+          </div>
+          <div className={styles.description}>{shopDetails.description}</div>
         </div>
         <button className={styles.button} type="button">
           신청하기
