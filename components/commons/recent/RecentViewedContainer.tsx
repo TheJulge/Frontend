e03a5d7ea15a980c5b8679b92ff1719697824b98
jae6269 <jae6269@gmail.com>
2024-04-23 04:20:43 +0900

    [#33]feat: 최근에 본 공고 가져오기 , 렌더링 구현, 레이아웃 디자인

--- components/commons/recent/RecentViewedContainer.tsx
+++ components/commons/recent/RecentViewedContainer.tsx
@@ -1,16 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import { getNoticesFromLocalStorage } from '@/utils/watchedListStoreFunctions';
-import styles from './RecentViewedContainer.module.scss';
+import { SingleNoticeType } from '@/types/noticeTypes';
 import Card from '../card/Card';
+import styles from './RecentViewedContainer.module.scss';
 
 export default function RecentViewedContainer() {
-  let noticesArray = getNoticesFromLocalStorage();
+  const [notices, setNotices] = useState<SingleNoticeType[] | null>(null);
+
+  useEffect(() => {
+    const storageData = getNoticesFromLocalStorage();
+    setNotices(storageData);
+  }, []);
 
   return (
     <section className={styles.container}>
       <div className={styles.recentContainer}>
-        {noticesArray
-          ? noticesArray.map(notice => <Card noticeInfo={notice} />)
-          : null}
+        <h2>최근에 본 공고</h2>
+        <div className={styles.cards}>
+          {notices
+            ? notices.map(notice => (
+                <React.Fragment key={notice.item.id}>
+                  <Card noticeInfo={notice} />
+                </React.Fragment>
+              ))
+            : null}
+        </div>
       </div>
     </section>
   );
