50534d5a1f525ae7475c87a5e142644fe1ac665b
Sumin <csmxx99@naver.com>
2024-04-24 22:43:02 +0900

    [#61] chore: 주석 추가

--- utils/findCookieValue.ts
+++ utils/findCookieValue.ts
@@ -1,3 +1,9 @@
+/**
+ * 쿠키는 통으로 하나인 문자열 이라 키 값을 기준으로 잘라서 사용하는 쿠키 커터
+ * @param cookies
+ * @param key
+ */
+
 export default function findCookieValue(cookies: string, key: string): string {
   const cookieList: string[] = cookies.split('; ');
   const cookieMap: { [key: string]: string } = {};
--- utils/setServerSideCookie.ts
+++ utils/setServerSideCookie.ts
@@ -2,6 +2,11 @@ import { ssrInstance } from '@/libs';
 import { GetServerSidePropsContext } from 'next';
 import findCookieValue from './findCookieValue';
 
+/**
+ * getServerSideProps 사용 시 instance의 Authorization header에 쿠키값 넣어주는 함수
+ * @param context getServerSideProps의 context 넣기
+ */
+
 export function setServerSideCookie(context: GetServerSidePropsContext) {
   const cookies = context.req.headers.cookie;
 
