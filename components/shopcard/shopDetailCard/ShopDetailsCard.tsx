dec0ef2958552b743f2f1e0bdf9d9d04e08c54e5
jae6269 <jae6269@gmail.com>
2024-04-26 02:29:48 +0900

    [#57]chore: mock데이터 삭제, 데이터연결 및 intersection observer 설치

--- components/shopcard/shopDetailCard/ShopDetailsCard.tsx
+++ components/shopcard/shopDetailCard/ShopDetailsCard.tsx
@@ -3,95 +3,38 @@ import LocationIcon from '@/public/images/card/locationIcon.svg';
 import styles from './ShopDetailsCard.module.scss';
 import Link from 'next/link';
 
-const MOCK = {
-  item: {
-    id: '4490151c-5217-4157-b072-9c37b05bed47',
-    name: '진주회관',
-    category: '한식',
-    address1: '서울시 중구',
-    address2: '세종대로11길 26',
-    description: '콩국수 맛집 인정따리',
-    imageUrl:
-      'https://bootcamp-project-api.s3.ap-northeast-2.amazonaws.com/0-1/the-julge/1bdb43c8-ff08-4a46-81b0-7f91efced98c-jinju4.png',
-    originalHourlyPay: 10000,
-    user: {
-      item: {
-        id: '4e560aa8-ae5a-40e1-a6e0-2a1e8b866d17',
-        email: 'test-employer1@codeit.com',
-        type: 'employer',
-      },
-      href: '/api/0-1/the-julge/users/4e560aa8-ae5a-40e1-a6e0-2a1e8b866d17',
-    },
-  },
-  links: [
-    {
-      rel: 'self',
-      description: '가게 정보',
-      method: 'GET',
-      href: '/api/0-1/the-julge/shops/4490151c-5217-4157-b072-9c37b05bed47',
-    },
-    {
-      rel: 'update',
-      description: '가게 정보 수정',
-      method: 'PUT',
-      href: '/api/0-1/the-julge/shops/4490151c-5217-4157-b072-9c37b05bed47',
-      body: {
-        name: 'string',
-        category: '한식 | 중식 | 일식 | 양식 | 분식 | 카페 | 편의점 | 기타',
-        address1:
-          '서울시 종로구 | 서울시 중구 | 서울시 용산구 | 서울시 성동구 | 서울시 광진구 | 서울시 동대문구 | 서울시 중랑구 | 서울시 성북구 | 서울시 강북구 | 서울시 도봉구 | 서울시 노원구 | 서울시 은평구 | 서울시 서대문구 | 서울시 마포구 | 서울시 양천구 | 서울시 강서구 | 서울시 구로구 | 서울시 금천구 | 서울시 영등포구 | 서울시 동작구 | 서울시 관악구 | 서울시 서초구 | 서울시 강남구 | 서울시 송파구 | 서울시 강동구',
-        address2: 'string',
-        description: 'string',
-        imageUrl: 'string',
-        originalHourlyPay: 'number',
-      },
-    },
-    {
-      rel: 'user',
-      description: '가게 주인 정보',
-      method: 'GET',
-      href: '/api/0-1/the-julge/users/4e560aa8-ae5a-40e1-a6e0-2a1e8b866d17',
-    },
-    {
-      rel: 'notices',
-      description: '공고 목록',
-      method: 'GET',
-      href: '/api/0-1/the-julge/shops/4490151c-5217-4157-b072-9c37b05bed47/notices',
-      query: {
-        offset: 'undefined | number',
-        limit: 'undefined | number',
-      },
-    },
-  ],
-};
 interface ShopDetailsCardProps {
-  id: string;
+  shopId: string;
+  shopData: any;
 }
-export default function ShopDetailsCard({ id }: ShopDetailsCardProps) {
+export default function ShopDetailsCard({
+  shopId,
+  shopData,
+}: ShopDetailsCardProps) {
   return (
     <div className={styles.container}>
       <div className={styles.cardImg}>
-        <Image src={MOCK.item.imageUrl} alt="shop image" fill />
+        <Image src={shopData.item.imageUrl} alt="shop image" fill />
       </div>
       <div className={styles.contents}>
         <div className={styles.information}>
           <div className={styles.title}>
             <p>식당</p>
-            <span>{MOCK.item.name}</span>
+            <span>{shopData.item.name}</span>
           </div>
           <div className={styles.location}>
             <LocationIcon />
-            <p>{MOCK.item.address1}</p>
+            <p>{shopData.item.address1}</p>
           </div>
-          <p className={styles.description}>{MOCK.item.description}</p>
+          <p className={styles.description}>{shopData.item.description}</p>
         </div>
         <div className={styles.links}>
-          <Link className={styles.editLink} href={`/`}>
+          <Link className={styles.editLink} href={`/shops`}>
             편집하기
           </Link>
           <Link
             className={styles.noticeEnterLink}
-            href={`/shops/${id}/notices`}
+            href={`/shops/${shopId}/notices`}
           >
             공고 등록하기
           </Link>
--- pages/shops/[id]/index.tsx
+++ pages/shops/[id]/index.tsx
@@ -1,29 +1,41 @@
 import EmptyTable from '@/components/table/emptytable/EmptyTable';
 import { GetServerSideProps } from 'next';
 import ShopDetailsCard from '@/components/shopcard/shopDetailCard/ShopDetailsCard';
-import styles from './ShopDetails.module.scss';
 import { getShop } from '@/libs/shop';
+import { getShopNotices } from '@/libs/notice';
+import styles from './ShopDetails.module.scss';
 
 export const getServerSideProps: GetServerSideProps<any> = async context => {
   const { query } = context;
   const shopId = query.id as string;
-  const shopData = getShop({ shopId: shopId });
-  return { props: { shopId } };
+
+  const shopResponse = await getShop({ shopId: shopId });
+  const shopNoticesResponse = await getShopNotices({ shopId: shopId });
+
+  const shopData = shopResponse.data;
+  const shopNoticesData = shopNoticesResponse.data;
+  return { props: { shopId, shopData, shopNoticesData } };
 };
 
 interface ShopDetailsProps {
-  id: string;
+  shopId: string;
+  //타입정의 하기
+  shopData: any;
+  shopNoticesData: any;
 }
 
-export default function ShopDetails({ id }: ShopDetailsProps) {
-  console.log(id);
+export default function ShopDetails({
+  shopId,
+  shopData,
+  shopNoticesData,
+}: ShopDetailsProps) {
   const isShopData = true;
   return (
     <main className={styles.wrapper}>
       <div className={styles.myShopContainer}>
         <h1>내 가게</h1>
-        {isShopData ? (
-          <ShopDetailsCard id={id} />
+        {shopData ? (
+          <ShopDetailsCard shopId={shopId} shopData={shopData} />
         ) : (
           <EmptyTable
             text="내 가게를 소개하고 공고도 등록해 주세요."
