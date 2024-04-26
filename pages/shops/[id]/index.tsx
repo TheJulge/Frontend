53a35ceaaf296b54aba36f230946ca6640229b92
jae6269 <jae6269@gmail.com>
2024-04-26 22:39:25 +0900

    [#57]feat: 가게정보 상세 페이지 ssr 구현

--- pages/shops/[id]/index.tsx
+++ pages/shops/[id]/index.tsx
@@ -1,67 +1,54 @@
-import { useInView } from 'react-intersection-observer';
-import { useEffect, useState } from 'react';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
-import { getShop } from '@/libs/shop';
-import { getShopNotices } from '@/libs/notice';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { instance } from '@/libs';
 import EmptyTable from '@/components/table/emptytable/EmptyTable';
 import ShopDetailsCard from '@/components/shopcard/shopDetailCard/ShopDetailsCard';
-import styles from './ShopDetails.module.scss';
-import { shopData } from './shopDetailsMocks';
-import { shopNoticesData } from './shopDetailsMocks';
 import Card from '@/components/commons/card/Card';
-import { postApplication } from '@/libs/application';
+import { ShopDetailType } from '@/types/shopTypes';
+import { NoticesType } from '@/types/noticeTypes';
+import styles from './ShopDetails.module.scss';
 
-// export const getServerSideProps: GetServerSideProps<any> = async context => {
-//   const { query } = context;
-//   const shopId = query.id as string;
-//   let shopData = null;
-//   let shopNoticesData = null;
-//   try {
-//     const shopResponse = await getShop({ shopId: shopId });
-//     if (shopResponse.status !== 200) {
-//       throw new Error('shop data fetch error');
-//     }
-//     shopData = await shopResponse.data;
-//   } catch (error) {
-//     shopData = { error: 'fetch error' };
-//   }
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { query } = context;
+  const shopId = query.id as string;
 
-//   try {
-//     const shopNoticesResponse = await getShopNotices({ shopId: shopId });
-//     if (shopNoticesResponse.status !== 200) {
-//       throw new Error('shop notices data fetch error');
-//     }
-//     shopNoticesData = shopNoticesResponse.data;
-//   } catch (error) {
-//     shopNoticesData = { error: 'fetch error' };
-//   }
+  const shopResponse = await instance.get(`/shops/${shopId}`);
+  const shopNoticesResponse = await instance.get(
+    `/shops/${shopId}/notices?offset=0&limit=6`,
+  );
 
-//   return { props: { shopId, shopData, shopNoticesData } };
-// };
+  const shopData = await shopResponse.data;
+  const shopNoticesData = shopNoticesResponse.data;
+
+  return { props: { shopId, shopData, shopNoticesData } };
+};
 
 interface ShopDetailsProps {
   shopId: string;
-  //타입정의 하기
-  shopData: any;
-  shopNoticesData: any;
+  shopData: ShopDetailType;
+  shopNoticesData: NoticesType;
 }
 
-export default function ShopDetails(
-  {
-    // shopId,
-    // shopData,
-    // shopNoticesData,
-  }: ShopDetailsProps,
-) {
+export default function ShopDetails({
+  shopId,
+  shopData,
+  shopNoticesData,
+}: ShopDetailsProps) {
+  const [noticeList, setNoticeList] = useState(shopNoticesData.items);
   const [page, setPage] = useState(0);
+  const router = useRouter();
   const [ref, inView] = useInView();
-  const shopId = '4490151c-5217-4157-b072-9c37b05bed47';
 
   useEffect(() => {
     if (inView) {
+      console.log(true);
+    } else {
+      console.log(false);
     }
   }, [inView]);
+
   return (
     <>
       <main className={styles.wrapper}>
@@ -78,6 +65,7 @@ export default function ShopDetails(
               <EmptyTable
                 text="공고를 등록해 보세요."
                 buttonText="공고 등록하기"
+                link=""
               />
             </>
           ) : (
@@ -88,11 +76,11 @@ export default function ShopDetails(
                   <Card noticeInfo={notice} />
                 ))} */}
               </div>
-              <div ref={ref} />
             </>
           )}
         </div>
       </section>
+      <div ref={ref} />
     </>
   );
 }
