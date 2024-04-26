733a5f479c3b5e8295decbfd3b962c518bd31b83
jae6269 <jae6269@gmail.com>
2024-04-26 12:00:27 +0900

    [#57]feat: 내가 등록한 공고 데이터 mock연결, 공고 0개이면 emptyTable연결

--- pages/shops/[id]/index.tsx
+++ pages/shops/[id]/index.tsx
@@ -1,21 +1,40 @@
-import EmptyTable from '@/components/table/emptytable/EmptyTable';
 import { GetServerSideProps } from 'next';
-import ShopDetailsCard from '@/components/shopcard/shopDetailCard/ShopDetailsCard';
+import { useRouter } from 'next/router';
 import { getShop } from '@/libs/shop';
 import { getShopNotices } from '@/libs/notice';
+import EmptyTable from '@/components/table/emptytable/EmptyTable';
+import ShopDetailsCard from '@/components/shopcard/shopDetailCard/ShopDetailsCard';
 import styles from './ShopDetails.module.scss';
+import { shopData } from './shopDetailsMocks';
+import { shopNoticesData } from './shopDetailsMocks';
 
-export const getServerSideProps: GetServerSideProps<any> = async context => {
-  const { query } = context;
-  const shopId = query.id as string;
+// export const getServerSideProps: GetServerSideProps<any> = async context => {
+//   const { query } = context;
+//   const shopId = query.id as string;
+//   let shopData = null;
+//   let shopNoticesData = null;
+//   try {
+//     const shopResponse = await getShop({ shopId: shopId });
+//     if (shopResponse.status !== 200) {
+//       throw new Error('shop data fetch error');
+//     }
+//     shopData = await shopResponse.data;
+//   } catch (error) {
+//     shopData = { error: 'fetch error' };
+//   }
 
-  const shopResponse = await getShop({ shopId: shopId });
-  const shopNoticesResponse = await getShopNotices({ shopId: shopId });
+//   try {
+//     const shopNoticesResponse = await getShopNotices({ shopId: shopId });
+//     if (shopNoticesResponse.status !== 200) {
+//       throw new Error('shop notices data fetch error');
+//     }
+//     shopNoticesData = shopNoticesResponse.data;
+//   } catch (error) {
+//     shopNoticesData = { error: 'fetch error' };
+//   }
 
-  const shopData = shopResponse.data;
-  const shopNoticesData = shopNoticesResponse.data;
-  return { props: { shopId, shopData, shopNoticesData } };
-};
+//   return { props: { shopId, shopData, shopNoticesData } };
+// };
 
 interface ShopDetailsProps {
   shopId: string;
@@ -24,25 +43,36 @@ interface ShopDetailsProps {
   shopNoticesData: any;
 }
 
-export default function ShopDetails({
-  shopId,
-  shopData,
-  shopNoticesData,
-}: ShopDetailsProps) {
-  const isShopData = true;
+export default function ShopDetails(
+  {
+    // shopId,
+    // shopData,
+    // shopNoticesData,
+  }: ShopDetailsProps,
+) {
+  const shopId = '4490151c-5217-4157-b072-9c37b05bed47';
+
   return (
-    <main className={styles.wrapper}>
-      <div className={styles.myShopContainer}>
-        <h1>내 가게</h1>
-        {shopData ? (
-          <ShopDetailsCard shopId={shopId} shopData={shopData} />
-        ) : (
-          <EmptyTable
-            text="내 가게를 소개하고 공고도 등록해 주세요."
-            buttonText="가게 등록하기"
-          />
-        )}
-      </div>
-    </main>
+    <>
+      <main className={styles.wrapper}>
+        <div className={styles.myShopContainer}>
+          <h1>내 가게</h1>
+          {shopData && <ShopDetailsCard shopId={shopId} shopData={shopData} />}
+        </div>
+      </main>
+      <section className={styles.noticeListWrapper}>
+        <div className={styles.noticesContainer}>
+          <h2>등록한 공고</h2>
+          {shopNoticesData.count === 0 ? (
+            <EmptyTable
+              text="공고를 등록해 보세요."
+              buttonText="공고 등록하기"
+            />
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </section>
+    </>
   );
 }
